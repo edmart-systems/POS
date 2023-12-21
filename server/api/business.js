c304e0d641b9caac201ff6177480e52892f30910
gastoncodes <nahurirabriangaston@2019>
2023-12-21 11:49:09 +0300

    login to business

--- server/api/business.js
+++ server/api/business.js
@@ -88,4 +88,75 @@ router.post("/business", async (req, res) => {
   }
 });
 
+//logging into a business
+router.post("/login/business", async (req, res) => {
+  const { bss_email, bss_password } = req.body;
+
+  //email verification
+  if (verifyEmail(bss_email)) {
+    //find business by its email
+    const business = await Business.findOne({
+      where: { bss_email: bss_email },
+    });
+    if (business) {
+      //decrypt business password
+      const original_password = cryptoJs.AES.decrypt(
+        business.bss_password,
+        process.env.PARSE_SECRET
+      ).toString(cryptoJs.enc.Utf8);
+      //verify paassword match
+      if (original_password === bss_password) {
+        const accessToken = jwt.sign(
+          {
+            bss_email: business.bss_email,
+            id: business.id,
+            bss_name: business.bss_name,
+            bss_country: business.bss_country,
+          },
+          process.env.ACCESS_SECRET,
+          {
+            expiresIn: "1h",
+          }
+        );
+        //generate a refresh token
+        const refreshToken = jwt.sign(
+          {
+            bss_email: business.bss_email,
+            id: business.id,
+            bss_name: business.bss_name,
+            bss_country: business.bss_country,
+          },
+          process.env.REFRESH_SECRET,
+          {
+            expiresIn: "5h",
+          }
+        );
+        //login response
+        res.json({
+          status: true,
+          data: "Login successful",
+          user: business,
+          accessToken: accessToken,
+          refreshToken: refreshToken,
+        });
+      } else {
+        res.json({
+          status: false,
+          data: "wrong password",
+        });
+      }
+    } else {
+      res.json({
+        status: false,
+        data: "business does not exist",
+      });
+    }
+  } else {
+    res.json({
+      status: false,
+      data: "wrong email format",
+    });
+  }
+});
+
 module.exports = router;
